e768207a851ea47a487a91f142f67af9e96b5e15
Jonata Silva <jonatasilva118@gmail.com>
2023-01-14 15:06:45 -0300

    1st commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+function active360(){
+    const url = window.location.origin; // Aqui você vai ter como retorno o caminho do cocumento, completo.
+    const imgOfSofa = document.getElementById('statcSofa'); // Aqui estou coletando as imagens.
+    const imgBtn = document.getElementById('img-btn-select');
+// Condicional para alternar entre as imagens.
+    if (imgBtn.src == `${url}/Leave.png`) {
+        imgBtn.src = 'Vector.png'; 
+        imgOfSofa.src = 'image 2.png';
+        return;
+    };
+// troca de imagens ao clicar.
+    imgBtn.src = 'Leave.png';
+
+    imgOfSofa.src = 'sofa-360.gif';
+    imgOfSofa.width = '449';
+};
